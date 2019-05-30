b26edcaaf855228cc16ad6315a2484e92bfa21b8
Khristian Punzalan <khrispunzalan@gmail.com>
2019-05-30 11:44:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -34,5 +34,5 @@ def last_element_with_array_methods(last_country)
   last_country = @south_east_asia[-1]
 end
 
-def 
+def length_of_array()
    @programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
